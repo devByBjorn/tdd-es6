fb4ea7ef44e53151f956f8a80c2c5770b6ad9706
devByBjorn <devbybjorn@gmail.com>
2020-06-29 13:50:57 +0200

    install and use ramda

--- src/anagrams.js
+++ src/anagrams.js
@@ -1,3 +1,5 @@
+import R from 'ramda'
+
 export const letterCount = arr => {
   return arr.reduce((acc, x) => {
     const count = acc[x] || 0
@@ -5,46 +7,52 @@ export const letterCount = arr => {
   }, {})
 }
 
-export const formString = str => str.split(' ').join('').split('')
-
-export const sortKeys = obj =>
-  Object.keys(obj)
-    .slice()
-    .sort((a, b) => {
-      let x = a.toLowerCase()
-      let y = b.toLowerCase()
-      if (x > y) return 1
-      if (y > x) return -1
-      return 0
-    })
-
-export const compareKeys = (obj1, obj2) => {
-  const arr1 = sortKeys(obj1)
-  const arr2 = sortKeys(obj2)
-
-  if (arr1.length === arr2.length) {
-    const compareArrs = arr1.find((item, i) => item.toLowerCase() !== arr2[i].toLowerCase())
-    return compareArrs ? false : true
-  } else {
-    return false
-  }
-}
-
-export const sumObjValues = obj =>
-  Object.values(obj).reduce((acc, value) => acc + value, 0)
-
-export const compareObjValues = (obj1, obj2) => {
-  const sum1 = sumObjValues(obj1)
-  const sum2 = sumObjValues(obj2)
-
-  return sum1 !== sum2 ? false : true
-}
+export const formString = str =>
+  str.split(' ').join('').split('')
 
 export const isAnagram = (str1, str2) => {
   const arr1 = formString(str1)
   const arr2 = formString(str2)
+
+  if (arr1.length !== arr2.length) return false
+
   const count1 = letterCount(arr1)
   const count2 = letterCount(arr2)
 
-  return compareKeys(count1, count2) && compareObjValues(count1, count2)
+  return R.equals(count1, count2)
 }
+
+// export const sortKeys = obj =>
+//   Object.keys(obj)
+//     .slice()
+//     .sort((a, b) => {
+//       let x = a.toLowerCase()
+//       let y = b.toLowerCase()
+//       if (x > y) return 1
+//       if (y > x) return -1
+//       return 0
+//     })
+
+// export const compareKeys = (obj1, obj2) => {
+//   const arr1 = sortKeys(obj1)
+//   const arr2 = sortKeys(obj2)
+
+//   if (arr1.length === arr2.length) {
+//     const compareArrs = arr1.find((item, i) => item.toLowerCase() !== arr2[i].toLowerCase())
+//     return compareArrs ? false : true
+//   } else {
+//     return false
+//   }
+// }
+
+// export const sumObjValues = obj =>
+//   Object.values(obj).reduce((acc, value) => acc + value, 0)
+
+// export const compareObjValues = (obj1, obj2) => {
+//   const sum1 = sumObjValues(obj1)
+//   const sum2 = sumObjValues(obj2)
+
+//   return sum1 !== sum2 ? false : true
+// }
+
+
--- src/anagrams.test.js
+++ src/anagrams.test.js
@@ -27,87 +27,102 @@ describe('formString functionality', () => {
   })
 })
 
-describe('sortKeys functionality', () => {
-  it('should take an object as input and return the objects keys in a sorted array', () => {
-    const obj = { a: 1, e: 2, n: 1, o: 1, p: 1 }
-    const expected = ['a', 'e', 'n', 'o', 'p']
-    const actual = sortKeys(obj)
-    expect(actual).to.deep.equal(expected)
-  })
-
-  it('should return a correctly sorted array when capitals and minor letters are combined', () => {
-    const obj = { c: 1, A: 3, T: 1 }
-    const expected = ['A', 'c', 'T']
-    const actual = sortKeys(obj)
-    expect(actual).to.deep.equal(expected)
-  })
-})
-
-describe('compareKeys functionality', () => {
-  it('should return false when comparing two objects that do not have the same key', () => {
-    const obj1 = { a: 1, e: 2, n: 1, o: 1, p: 1 }
-    const obj2 = { c: 1, A: 3, T: 1 }
-    const expected = false
-    const actual = compareKeys(obj1, obj2)
-    expect(actual).to.deep.equal(expected)
-  })
-
-  it('should return true when compareing objects with the same keys', () => {
-    const obj1 = { c: 1, A: 3, T: 1 }
-    const obj2 = { c: 1, A: 9, T: 1 }
-    const expected = true
-    const actual = compareKeys(obj1, obj2)
-    expect(actual).to.deep.equal(expected)
-  })
-
-  it('should return true when comparing objects with the same keys, capitals and minor letters should be counted as the same', () => {
-    const obj1 = { c: 1, A: 3, T: 1 }
-    const obj2 = { c: 1, A: 9, t: 1 }
-    const expected = true
-    const actual = compareKeys(obj1, obj2)
-    expect(actual).to.deep.equal(expected)
-  })
-})
-
-describe('sumObjValues functionality', () => {
-  it('should return the sum of an objects values', () => {
-    const obj = { c: 1, a: 1, t: 5 }
-    const expected = 7
-    const actual = sumObjValues(obj)
-    expect(actual).to.deep.equal(expected)
-  })
-})
-
-describe('compareObjValues functionality', () => {
-  it('should return true if the sum of two objects values are the same', () => {
-    const obj1 = { c: 1, a: 1, t: 5 }
-    const obj2 = { d: 1, a: 1, k: 5 }
-    const expected = true
-    const actual = compareObjValues(obj1, obj2)
-    expect(actual).to.deep.equal(expected)
-  })
-  it('should return false if teh sum of two ojbects values are not the samee', () => {
-    const obj1 = { c: 1, a: 1, t: 5 }
-    const obj2 = { d: 1, a: 1, k: 8 }
-    const expected = false
-    const actual = compareObjValues(obj1, obj2)
-    expect(actual).to.deep.equal(expected)
-  })
-})
-
 describe('isAnagram functionality', () => {
   it('should return true if two strings are anagrams of each other', () => {
-    const str1 = 'hello world'
-    const str2 = 'HelloWorld'
+    const str1 = 'silent'
+    const str2 = 'lisent'
     const expected = true
     const actual = isAnagram(str1, str2)
-    expect(actual).to.deep.eq(expected)
+    expect(actual).to.equal(expected)
   })
-  it('should return false if two string are not anagrams of each', () => {
-    const str1 = 'hello worlds'
-    const str2 = 'hello world'
+  it('should return true if one string has spaces in it if letters form an anagram of the orther word', () => {
+    const str1 = 'eleven plus two'
+    const str2 = 'twelveplusone'
+    const expected = true
+    const actual = isAnagram(str1, str2)
+    expect(actual).to.equal(expected)
+  })
+  it('should return false if one string has more letters than the other', () => {
+    const str1 = 'silentt'
+    const str2 = 'lisent'
     const expected = false
     const actual = isAnagram(str1, str2)
-    expect(actual).to.deep.eq(expected)
+    expect(actual).to.equal(expected)
+  })
+  it('should reutrn false if strings has the same length but different quantity of certain letter', () => {
+    const str1 = 'sillent'
+    const str2 = 'lissent'
+    const expected = false
+    const actual = isAnagram(str1, str2)
+    expect(actual).to.equal(expected)
   })
 })
+
+
+// describe('sortKeys functionality', () => {
+//   it('should take an object as input and return the objects keys in a sorted array', () => {
+//     const obj = { a: 1, e: 2, n: 1, o: 1, p: 1 }
+//     const expected = ['a', 'e', 'n', 'o', 'p']
+//     const actual = sortKeys(obj)
+//     expect(actual).to.deep.equal(expected)
+//   })
+
+//   it('should return a correctly sorted array when capitals and minor letters are combined', () => {
+//     const obj = { c: 1, A: 3, T: 1 }
+//     const expected = ['A', 'c', 'T']
+//     const actual = sortKeys(obj)
+//     expect(actual).to.deep.equal(expected)
+//   })
+// })
+
+// describe('compareKeys functionality', () => {
+//   it('should return false when comparing two objects that do not have the same key', () => {
+//     const obj1 = { a: 1, e: 2, n: 1, o: 1, p: 1 }
+//     const obj2 = { c: 1, A: 3, T: 1 }
+//     const expected = false
+//     const actual = compareKeys(obj1, obj2)
+//     expect(actual).to.equal(expected)
+//   })
+
+//   it('should return true when compareing objects with the same keys', () => {
+//     const obj1 = { c: 1, A: 3, T: 1 }
+//     const obj2 = { c: 1, A: 9, T: 1 }
+//     const expected = true
+//     const actual = compareKeys(obj1, obj2)
+//     expect(actual).to.equal(expected)
+//   })
+
+//   it('should return true when comparing objects with the same keys, capitals and minor letters should be counted as the same', () => {
+//     const obj1 = { c: 1, A: 3, T: 1 }
+//     const obj2 = { c: 1, A: 9, t: 1 }
+//     const expected = true
+//     const actual = compareKeys(obj1, obj2)
+//     expect(actual).to.equal(expected)
+//   })
+// })
+
+// describe('sumObjValues functionality', () => {
+//   it('should return the sum of an objects values', () => {
+//     const obj = { c: 1, a: 1, t: 5 }
+//     const expected = 7
+//     const actual = sumObjValues(obj)
+//     expect(actual).to.equal(expected)
+//   })
+// })
+
+// describe('compareObjValues functionality', () => {
+//   it('should return true if the sum of two objects values are the same', () => {
+//     const obj1 = { c: 1, a: 1, t: 5 }
+//     const obj2 = { d: 1, a: 1, k: 5 }
+//     const expected = true
+//     const actual = compareObjValues(obj1, obj2)
+//     expect(actual).to.equal(expected)
+//   })
+//   it('should return false if teh sum of two ojbects values are not the samee', () => {
+//     const obj1 = { c: 1, a: 1, t: 5 }
+//     const obj2 = { d: 1, a: 1, k: 8 }
+//     const expected = false
+//     const actual = compareObjValues(obj1, obj2)
+//     expect(actual).to.equal(expected)
+//   })
+// })
